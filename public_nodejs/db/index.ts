58d9c41a5d334d2be641debb7b9f69798d22b533
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2025-12-07 09:57:46 +0100

    move to better sqlite

--- public_nodejs/db/index.ts
+++ public_nodejs/db/index.ts
@@ -1,6 +1,5 @@
-import { type Client, createClient } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
-
+import Database from "better-sqlite3";
+import { drizzle } from "drizzle-orm/better-sqlite3";
 import { env } from "@/env";
 import * as schema from "./schema";
 
@@ -9,11 +8,10 @@ import * as schema from "./schema";
  * update.
  */
 const globalForDb = globalThis as unknown as {
-	client: Client | undefined;
+	client: ReturnType<typeof Database> | undefined;
 };
 
-export const client =
-	globalForDb.client ?? createClient({ url: env.DATABASE_URL });
+export const client = globalForDb.client ?? Database(env.DATABASE_URL);
 if (env.NODE_ENV !== "production") globalForDb.client = client;
 
 export const db = drizzle(client, { schema });
